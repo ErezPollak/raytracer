52d44fb53d5b39f7c4fc86c692a2f9bf384997de
אלירן סלמה <esalama@g.jct.ac.il>
2022-02-21 21:07:08 +0200

    Point final

--- ISE5782_8995_1159/src/primitives/Point.java
+++ ISE5782_8995_1159/src/primitives/Point.java
@@ -1,3 +1,4 @@
+import math;
 package primitives;
 
 public class Point {
@@ -9,5 +10,14 @@ public class Point {
      Double3 D3 = this.xyz.subtract(p.xyz);
       return new Vector(D3.d1,D3.d2,D3.d3);
    }
-   Point add()
+   Point add(Vector v){
+       Double3 D3 = this.xyz.add(v.xyz);
+       return new Point(D3.d1,D3.d2,D3.d3);
+   }
+   double distanceSquared(Point p){
+       return ((this.xyz.d1 - p.xyz.d1)*(this.xyz.d1 - p.xyz.d1) + (this.xyz.d2 - p.xyz.d2)*(this.xyz.d2 - p.xyz.d2) + (this.xyz.d3 - p.xyz.d3)*(this.xyz.d3 - p.xyz.d3));
+   }
+   double distance(Point p){
+       return sqrt.sqrt(this.distanceSquared(p));
+   }
 }
