5d152a501e908c8a2156801206d1b52550de3b14
אלירן סלמה <esalama@g.jct.ac.il>
2022-03-23 11:53:29 +0200

    Targil3 - Geometries + test for geometries

--- ISE5782_8995_1159/src/geometries/Geometries.java
+++ ISE5782_8995_1159/src/geometries/Geometries.java
@@ -12,35 +12,54 @@ import java.util.List;
  * using the composite design pattern
  */
 public class Geometries implements Intersectable {
-    private List<Intersectable> listOfIntersectable;
+    private List<Intersectable> listOfGeometries;
 
     /**
      * Default constructor for Geometries
      */
     public Geometries() {
-        listOfIntersectable = new LinkedList();
+        listOfGeometries = new LinkedList();
     }
 
     /**
-     *
+     * Constructor for Geometries
      * @param geometries
      */
     public Geometries(Intersectable... geometries)
     {
-
+        this();
+        add(geometries);
     }
 
     /**
-     *
+     * Add geometry to the collection
      * @param geometries
      */
     public void add(Intersectable... geometries)
     {
-
+        for (var item: geometries) {
+            listOfGeometries.add(item);
+        }
     }
 
+    /**
+     * Finds all the intersections of the given ray with
+     * the geometries ,and return a list of those points of intersection.
+     * @param ray
+     * @return
+     */
     @Override
     public List<Point> findIntersections(Ray ray) {
-        return null;
+        List<Point> result = null; // Create empty list of the points
+        for(var item:listOfGeometries){
+            List<Point> itemPoints = item.findIntersections(ray); // For each geometry, find it intersection with the given ray,
+            if(itemPoints != null){                               // and create a list of them.
+                if(result==null){
+                    result = new LinkedList(); // If the list of all intersections with all geometries is empty, create the list.
+                }
+                result.addAll(itemPoints); // If there are intersections, add all the intersections of the geometrie to the list
+            }                              // of all intersections with all geometries.
+        }
+        return result;
     }
 }
--- ISE5782_8995_1159/unittests/geometries/GeometriesTest.java
+++ ISE5782_8995_1159/unittests/geometries/GeometriesTest.java
@@ -9,4 +9,59 @@ import primitives.Vector;
  */
 class GeometriesTest {
 
+    // Create 3 geometries collection (1 from each geometrie) to test intersection of ray with them.
+    Geometries geo = new Geometries(
+            new Sphere (2, new Point(1,0.5,1)),
+            new Plane(
+                    new Point(-2,0,0),
+                    new Point(0,0,4),
+                    new Point(0,-2,0)),
+            new Triangle(
+                    new Point(1,0,0),
+                    new Point(0.1,0.5,2.5),
+                    new Point(-2,0,0)));
+
+    // empty collection for empty test.
+    Geometries emptyGeo = null;
+
+    // ray for calculations.
+    Ray ray;
+
+    /**
+     * Test method for {@link geometries.Geometries#findIntersections(primitives.Ray)}.
+     */
+    @Test
+    void testIntersections()
+    {
+        // =============== Boundary Values Tests ==================
+
+        // TC01: Empty collection - should return null (0 points)
+        ray = new Ray(new Point(6,2,2), new Vector(0,3,1));
+        assertNull(emptyGeo.findIntersections(ray),
+                "TC01 failed: expected no intersection points.");
+
+        // TC02: There are no intersections with the ray (0 points)
+        ray = new Ray(new Point(6,2,2), new Vector(0,3,1));
+        assertNull(geo.findIntersections(ray),
+                "TC02 failed: expected no intersection points.");
+
+        // TC03: Only one geometrie has intersection with the ray.
+        ray = new Ray(new Point(0,-10,5), new Vector(0,10,-1));
+        assertEquals(1,geo.findIntersections(ray).size(),
+                "TC03 failed: expected 1 intersection points.");
+
+        // TC04: Ray intersect all the geometries.
+        ray = new Ray(new Point(0,-10,5), new Vector(0,10,-4));
+        assertEquals(4,geo.findIntersections(ray).size(),
+                "TC04 failed: expected 4 intersection points.");
+
+        // ============ Equivalence Partitions Tests ==============
+
+        // TC11:Ray intersect with some geometries.
+        ray = new Ray(new Point(0,-10,5), new Vector(1,10.5,-4));
+        assertEquals(3,geo.findIntersections(ray).size(),
+                "TC04 failed: expected 3 intersection points.");
+
+    }
+
 }
