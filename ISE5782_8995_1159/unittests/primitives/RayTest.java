fef885ab5211598252049fc2e90382d37a7ebde6
Erez Polak <pwlqrz@gmail.com>
2022-04-04 17:40:31 +0300

    Targil5 prat 3: closest point ray, function and tests

---
+++ ISE5782_8995_1159/unittests/primitives/RayTest.java
@@ -0,0 +1,41 @@
+package primitives;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RayTest {
+
+    /**
+     * testing the function that returns the clothest point from a list.
+     * @author Erez Polak
+     */
+    @Test
+    void findClosestPoint() {
+
+        Point p1 = new Point(0,3,4) , p2 = new Point(0,2,3), p3 = new Point(0,4,5);
+        List<Point> list = List.of(p1,p2,p3);
+        Ray r = new Ray(new Point(0,1,2) , new Vector(0,1,1));
+
+        // ============ Equivalence Partitions Tests ==============
+
+        assertEquals(p2 , r.findClosestPoint(list) , "Equivalence Partitions Tests Failed.");
+
+        // =============== Boundary Values Tests ==================
+
+        //TC01: the list is empty:
+        List<Point> emptyList = List.of();
+        assertEquals(null , r.findClosestPoint(emptyList) , "Boundary Values Tests TC01 Failed.");
+
+        //TC02: the first point is required:
+        list = List.of(p2,p1,p3);
+        assertEquals(p2 , r.findClosestPoint(list) , "Boundary Values Tests TC02 Failed.");
+
+        //TC03: the last point is required:
+        list = List.of(p3,p1,p2);
+        assertEquals(p2 , r.findClosestPoint(list) , "Boundary Values Tests TC03 Failed.");
+
+    }
+}
