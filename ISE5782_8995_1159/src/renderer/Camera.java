bc44445a86869357455706b95e8c997b665e0a8c
אלירן סלמה <esalama@g.jct.ac.il>
2022-04-06 11:07:58 +0300

    Targil5 - level 4 (camera functions)

--- ISE5782_8995_1159/src/renderer/Camera.java
+++ ISE5782_8995_1159/src/renderer/Camera.java
@@ -3,6 +3,10 @@ package renderer;
 import primitives.Point;
 import primitives.Ray;
 import primitives.Vector;
+import primitives.Color;
+
+import java.util.MissingResourceException;
+
 import static primitives.Util.*;
 
 public class Camera {
@@ -12,6 +16,8 @@ public class Camera {
     private Vector up;
     private Vector to;
     private Vector right;
+    private ImageWriter imageWriter;
+    private RayTracerBase rayTracer;
 
     ////view plain properties.////
     private double height;
@@ -107,5 +113,135 @@ public class Camera {
         return new Ray(location,vIJ);
     }
 
+    /**
+     * Set for image writer, and return the object itself.
+     * @param imageWriter
+     */
+    public Camera setImageWriter(ImageWriter imageWriter) {
+        this.imageWriter = imageWriter;
+        return this;
+    }
 
+    /**
+     * Set for ray trace, and return the object itself.
+     * @param rayTraceBase
+     */
+    public Camera setRayTraceBase(RayTracerBase _rayTracer) {
+        this.rayTracer = _rayTrace;
+        return this;
+    }
+
+    /**
+     * Create an image according to the colors and rays.
+     * in addition, checks if all the fields are not empty.
+     */
+    public void renderImage(){
+        try {
+            // if image writer is empty throw exception.
+            if (imageWriter == null) {
+                throw new MissingResourceException("missing resource", ImageWriter.class.getName(), "");
+            }
+
+            // if ray tracer is empty throw exception.
+            if (rayTracer == null) {
+                throw new MissingResourceException("missing resource", RayTracerBase.class.getName(), "");
+            }
+
+            // if image location is empty throw exception.
+            if (location == null) {
+                throw new MissingResourceException("missing resource", Point.class.getName(), "");
+            }
+
+            // if vector 'up' is empty throw exception.
+            if (up == null) {
+                throw new MissingResourceException("missing resource", Vector.class.getName(), "");
+            }
+
+            // if vector 'to' is empty throw exception.
+            if (to == null) {
+                throw new MissingResourceException("missing resource", Vector.class.getName(), "");
+            }
+
+            // if vector 'right' is empty throw exception.
+            if (right == null) {
+                throw new MissingResourceException("missing resource", Vector.class.getName(), "");
+            }
+            //           cant compare double to null.
+            //           if(height == null){
+            //               throw new MissingResourceException("missing resource", double.class.getName(),"");
+            //           }
+            //           if(width == null){
+            //               throw new MissingResourceException("missing resource", double.class.getName(),"");
+            //           }
+            //           if(distance == null){
+            //               throw new MissingResourceException("missing resource", double.class.getName(),"");
+            //           }
+            int Nx = imageWriter.getNx(), Ny = imageWriter.getNy();
+
+            //Go all over the matrix, and create ray and get color for each pixel.
+            for (int i = 0; i < Nx; i++) {
+                for (int j = 0; j < Ny; j++) {
+                    Color pixelColor = castRay(Nx, Ny, j, i);
+                    imageWriter.writePixel(i,j,pixelColor);
+                }
+            }
+        }
+        catch (MissingResourceException e) {
+            throw new UnsupportedOperationException("Missing resources in order to create the image"
+                    + e.getClassName());
+        }
+    }
+
+    /**
+     * The function calculates the color of the given pixel, according to the ray.
+     * @param nX  represents the amount of columns, the  width of a row.
+     * @param nY represents the amount for rows, the height of a column.
+     * the index of the pixel on the view plane.
+     * @param col represents the column.
+     * @param row represents the row.
+     * @return
+     */
+    private Color castRay(int nX, int nY, int col, int row){
+        Color pixelColor;
+        Ray ray = this.constructRay(nX,nY,col,row);
+        pixelColor = rayTracer.traceRay(ray);
+        return pixelColor;
+    }
+
+    /**
+     * The function paint the matrix's grid with the given color.
+     * @param interval
+     * @param color
+     */
+    public void printGrid(int interval, Color color){
+
+        // if the Image writer is empty, throw exception.
+        if(imageWriter == null){
+            throw new MissingResourceException("missing resource",ImageWriter.class.getName(),"");
+        }
+
+        int Nx = imageWriter.getNx(), Ny = imageWriter.getNy();
+
+        //Go all over the matrix, and paint the grid with the given color.
+        for(int i=0; i<Nx;i++){
+            for(int j=0; j<Ny;j++){
+                if(i%interval == 0 || j% interval ==0){
+                    imageWriter.writePixel(i,j,color);
+                }
+            }
+        }
+    }
+
+    /**
+     * Create png file of the image according
+     * to pixel color matrix in the directory of the project.
+     * If the image writer is missing - throw exception.
+     */
+    public void writeToImage()
+    {
+        if(imageWriter == null){
+            throw new MissingResourceException("missing resource",ImageWriter.class.getName(),"");
+        }
+        imageWriter.writeToImage();
+    }
 }
