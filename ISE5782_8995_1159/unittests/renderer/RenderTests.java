439b108006d1a137897bc058c9d2368a8188ed4d
Erez Polak <pwlqrz@gmail.com>
2022-04-07 10:02:10 +0300

    Targil4 prat 4: json bonus.

--- ISE5782_8995_1159/unittests/renderer/RenderTests.java
+++ ISE5782_8995_1159/unittests/renderer/RenderTests.java
@@ -1,5 +1,7 @@
 package renderer;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.junit.jupiter.api.Test;
 
 import lighting.AmbientLight;
@@ -7,6 +9,8 @@ import geometries.*;
 import primitives.*;
 import renderer.*;
 import scene.Scene;
+
+import java.io.FileReader;
 
 /**
  * Test rendering a basic image
@@ -53,6 +57,50 @@ public class RenderTests {
 
         // enter XML file name and parse from XML file into scene object
         // ...
+        String path = System.getProperty("user.dir");
+
+        JSONParser parser = new JSONParser();
+        try {
+
+            JSONObject jsonScene = (JSONObject) parser.parse(new FileReader(path + "\\Json\\stam.json"));
+
+            String ambientLight = (String) jsonScene.get("ambient-light");
+            Double3 dAmbientLight = new Double3(ambientLight);
+            scene.setAmbientLight(new AmbientLight(new Color(dAmbientLight),new Double3(1,1,1)));
+
+            String background = (String) jsonScene.get("background");
+            Double3 dBackground = new Double3(background);
+            scene.setBackground(new Color(dBackground));
+
+            JSONObject geometries = (JSONObject) jsonScene.get("geometries");
+            for (Object keyStr : geometries.keySet()) {
+                JSONObject keyValue = (JSONObject) geometries.get(keyStr);
+                if(keyStr.toString().startsWith("triangle")){
+
+                    String p0 = (String) keyValue.get("p0");
+                    String p1 = (String) keyValue.get("p1");
+                    String p2 = (String) keyValue.get("p2");
+
+                    Double3 d0 = new Double3(p0);
+                    Double3 d1 = new Double3(p1);
+                    Double3 d2 = new Double3(p2);
+
+                    scene.geometries.add(new Triangle(new Point(d0),new Point(d1),new Point(d2)));
+
+                }else if(keyStr.toString().startsWith("sphere")){
+                    int radius = Integer.parseInt((String) keyValue.get("radius"));
+                    String center = (String) keyValue.get("center");
+
+                    Double3 dCenter = new Double3(center);
+
+                    scene.geometries.add(new Sphere(radius ,new Point(dCenter)));
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
 
         Camera camera = new Camera(Point.ZERO, new Vector(0, 0, -1), new Vector(0, 1, 0)) //
                 .setVPDistance(100) //
